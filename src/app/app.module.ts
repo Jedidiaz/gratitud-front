197b319189d06fe7122f1cf637148531cf96045f
Jedidiaz <jedidiazfagundez@gmail.com>
2022-12-11 16:10:24 -0500

    arreglos home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { FooterComponent } from './shared/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  carousel: Array<any> = [];
+  users: Array<any> = [];
+  card = {image: '../../../assets/danielacorrea.png', name: 'Daniela Correa', description: 'Facilitadora de Conciencia'};
   constructor() { }
 
   ngOnInit(): void {
+    this.carousel = ['../../../assets/carousel.png', '../../../assets/carousel.png', '../../../assets/carousel.png'];
+    for(let i = 0; i < 8; i++){
+      this.users.push(this.card)
+    }
   }
 
 }
