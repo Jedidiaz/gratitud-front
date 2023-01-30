def5f5ae284950ef5aa99bfe7900bcae57953445
mariocsdev <63229039+mariocsdev@users.noreply.github.com>
2023-01-30 17:49:20 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,20 @@
 export const environment = {
-  production: true,
+  production: false,
   api: 'https://api.gratitud.link/api/',
+  subscription_paypalMes: 'P-61A016077N618563TMPGEN3Y',
+  subscription_paypalAño: 'P-1WH52851E6413843NMPE5G3Q',
+  donation_stripe1: 'price_1MQEomDqSAlgA2D1jS3a40OU',
+  donation_stripe2: 'price_1MQEdrDqSAlgA2D1ElK2gPmB',
+  donation_stripe3: 'price_1MQEiIDqSAlgA2D1H3Gz0Y8A',
+  subscription_stripeMes: 'price_1MTZEfDqSAlgA2D1GoJzFYXC',
+  subscription_stripeAño: 'price_1MQboBDqSAlgA2D1bJkoM9kv',
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
